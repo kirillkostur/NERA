4bfa5941f6c25239d953f9a89d697157173acaa1
kirillkostur <kirill.kostur98@gmail.com>
2025-09-20 02:21:30 +0300

    fix_target_obj_die_player

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -21,38 +21,38 @@ public class PlayerManager : MonoBehaviour
         if (isDead) return;
         isDead = true;
 
-        // 1) сохраняем ссылку на последнюю цель, затем снимаем таргет и выключаем систему
         Transform lastTarget = null;
         if (playerTargeting != null)
         {
             lastTarget = playerTargeting.CurrentTargetTransform;
-            playerTargeting.ClearTarget();      // теперь блокирует новые назначения
+            playerTargeting.ClearTarget();
             playerTargeting.enabled = false;
         }
 
-        // 2) гасим подсветку у последней цели (если вдруг осталась)
+        // ⛔ Сбрасываем ремонт, если был
         if (lastTarget != null)
         {
+            var repairable = lastTarget.GetComponent<RepairableObject>();
+            if (repairable != null)
+                repairable.CancelInteract();
+
             var spider = lastTarget.GetComponent<SpiderAI_NavMesh>();
             if (spider != null && spider.targetHighlightEffect != null)
                 spider.targetHighlightEffect.SetActive(false);
 
-            var repairable = lastTarget.GetComponent<RepairableObject>();
             if (repairable != null && repairable.targetHighlightEffect != null)
                 repairable.targetHighlightEffect.SetActive(false);
         }
 
-        // 3) отключаем управление и атаку
         if (playerController != null) playerController.enabled = false;
         if (playerAttack != null) playerAttack.enabled = false;
 
-        // 4) страхуемся: выключаем CharacterController
         var controller = GetComponent<CharacterController>();
         if (controller != null) controller.enabled = false;
 
-        // 5) чтобы нас не «видели» системы поиска
         gameObject.layer = LayerMask.NameToLayer("IgnoreRaycast");
     }
+
 
     private void ForceClearAllHighlights()
     {
